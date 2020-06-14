43214660065d887482e6ca3c5c09adc01091caf9
Aminat.A <aminatadesola@gmail.com>
2020-06-14 13:23:52 -0500

    added routes, server, and modules

---
+++ models/workout.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: String,
+        trim: true,
+        required: "String is Required"
+    },
+
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "String is Required"
+
+            }, 
+            name: {
+                type: String,
+                trim: true,
+                required: "String is Required"
+            },
+            duration : {
+                type: Number,
+                unique: true,
+                required: true
+            },
+            weight : {
+                type: Number,
+                unique: true,
+                required: true
+            },
+
+            reps: {
+                type: Number,
+                unique: true,
+                required: true
+            },
+           set : {
+                type: Number,
+                unique: true,
+                required: true
+            },
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
